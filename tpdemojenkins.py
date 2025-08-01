5e04e62ee666419d0ab852f74529130cda54315c
youssef <ybgdtraining@gmail.com>
2025-08-01 15:38:15 +0100

    Ajout fonction saluer - Version 1.2

--- tpdemojenkins.py
+++ tpdemojenkins.py
@@ -1,1 +1,8 @@
+#Version 1
 print("DEMO JENKINS - VERSION 1")
+#Version 2
+def saluer(nom):
+    print(f"Bonjour, {nom} !")
+
+if __name__ == "__main__":
+    saluer("EMO JENKINS - VERSION 2 -PULL ACTION")
